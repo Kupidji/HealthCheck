9dc78f9a924552e24bc90f25944c7b96208e15f8
goga-59 <egsad2004@gmail.com>
2023-06-06 17:46:58 +0500

    to do help

--- app/src/main/java/com/example/healthcheck/viewmodel/StartViewModel.kt
+++ app/src/main/java/com/example/healthcheck/viewmodel/StartViewModel.kt
@@ -3,5 +3,6 @@ package com.example.healthcheck.viewmodel
 import androidx.lifecycle.ViewModel
 
 class StartViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    val flagList = listOf()
 }
