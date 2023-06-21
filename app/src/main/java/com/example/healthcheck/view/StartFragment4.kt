37446e74b3658dce51c35577bf9d983246503873
Mika Barsic <misteriks788@gmail.com>
2023-06-21 20:57:32 +0300

    Приветствие 4,5,6.

--- app/src/main/java/com/example/healthcheck/view/StartFragment4.kt
+++ app/src/main/java/com/example/healthcheck/view/StartFragment4.kt
@@ -1,13 +1,16 @@
 package com.example.healthcheck.view
 
-import androidx.lifecycle.ViewModelProvider
+import android.R
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.healthcheck.R
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.example.healthcheck.databinding.FragmentStart4Binding
 import com.example.healthcheck.viewmodel.StartFragment4ViewModel
+
 
 class StartFragment4 : Fragment() {
 
@@ -16,17 +19,37 @@ class StartFragment4 : Fragment() {
     }
 
     private lateinit var viewModel: StartFragment4ViewModel
+    private lateinit var binding : FragmentStart4Binding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_start_4, container, false)
+        viewModel = ViewModelProvider(this).get(StartFragment4ViewModel::class.java)
+        binding = FragmentStart4Binding.inflate(inflater)
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(StartFragment4ViewModel::class.java)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val radioGroup = binding.radioGender
+        var gender = ""
+
+        val navigation = findNavController()
+
+        radioGroup.setOnCheckedChangeListener { group, checkedId ->
+            when (checkedId) {
+                -1 -> gender = ""
+                binding.rbMale.id -> gender = "man"
+                binding.rbFemale.id -> gender = "woman"
+            }
+        }
+
+        binding.next.setOnClickListener {
+            navigation.navigate(com.example.healthcheck.R.id.startFragment5)
+        }
+        
         // TODO: Use the ViewModel
     }
 
--- app/src/main/java/com/example/healthcheck/view/StartFragment5.kt
+++ app/src/main/java/com/example/healthcheck/view/StartFragment5.kt
@@ -6,7 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.healthcheck.R
+import android.widget.NumberPicker
+import androidx.navigation.fragment.findNavController
+import com.example.healthcheck.databinding.FragmentStart5Binding
 import com.example.healthcheck.viewmodel.StartFragment5ViewModel
 
 class StartFragment5 : Fragment() {
@@ -16,17 +18,38 @@ class StartFragment5 : Fragment() {
     }
 
     private lateinit var viewModel: StartFragment5ViewModel
+    private lateinit var binding : FragmentStart5Binding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_start_5, container, false)
+        viewModel = ViewModelProvider(this).get(StartFragment5ViewModel::class.java)
+        binding = FragmentStart5Binding.inflate(inflater)
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(StartFragment5ViewModel::class.java)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val navigation = findNavController()
+
+        val numberPicker0: NumberPicker = binding.numberPicker0
+        numberPicker0.setMaxValue(99)
+        numberPicker0.setMinValue(1)
+        numberPicker0.descendantFocusability = NumberPicker.FOCUS_BLOCK_DESCENDANTS
+        numberPicker0.setWrapSelectorWheel(false)
+
+        val numberPicker1: NumberPicker = binding.numberPicker1
+        numberPicker1.setMaxValue(200)
+        numberPicker1.setMinValue(5)
+        numberPicker1.descendantFocusability = NumberPicker.FOCUS_BLOCK_DESCENDANTS
+        numberPicker1.setWrapSelectorWheel(false)
+
+        binding.next.setOnClickListener {
+            navigation.navigate(com.example.healthcheck.R.id.startFragment6)
+        }
+
         // TODO: Use the ViewModel
     }
 
